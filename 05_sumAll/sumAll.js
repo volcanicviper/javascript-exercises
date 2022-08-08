58bfc43a7a83422e3655c990e8ab0e3f8bbf9079
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-08 20:47:27 +0200

    Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    for (arg of arguments) {
+        if (typeof arg != "number" || arg < 0) return "ERROR";
+    }
+    let sum = 0;
+    
+    if (x < y) {
+        for (; x <= y; ++x) {
+            sum += x;
+        }
+    }
+    else {
+        for (; x >= y; --x) {
+            sum += x;
+        }        
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
