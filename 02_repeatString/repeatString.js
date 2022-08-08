2a86f457bfc9abb96384ffb3455cbc322439a2b9
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-08 20:19:44 +0200

    Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let nString = "";
+    for (let i = 0; i < num; ++i) {
+        nString += string;
+    }
+    return nString;
 };
 
 // Do not edit below this line
