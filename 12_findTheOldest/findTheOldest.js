711a50607035fe1800021eee3d8133929980c070
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-22 20:00:37 +0200

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = (new Date()).getFullYear();
+    }
+    return death - birth;
+}
 
-};
+const findTheOldest = function(array) {
+    const sorted = array.sort(function(a, b) {
+        const last = getAge(a.yearOfBirth, a.yearOfDeath);
+        const next = getAge(b.yearOfBirth, b.yearOfDeath);
+        return last > next ? -1 : 1;
+    });
+    return sorted[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
