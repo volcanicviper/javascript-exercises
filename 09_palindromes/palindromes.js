c0b80752b0943aa9aa1f6f911cddda4b80b7c1bc
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-22 19:15:51 +0200

    Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+function noPunctuation(string) {
+    string = string.toLowerCase();
+    string = string.split('').filter(char => /[a-zA-Z]/.test(char));
+    return string.join('');
+}
 
+const palindromes = function(string) {
+    let noPunct = noPunctuation(string);
+    let reversed = "";
+    for (let i = noPunct.length - 1; i >= 0; --i) {
+        reversed += noPunct[i];
+    }
+    return reversed === noPunct;
 };
 
 // Do not edit below this line
