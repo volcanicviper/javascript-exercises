57ba7d4ad44e8fd4440fbf2e807e4de09afd5d94
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-22 19:03:34 +0200

    Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i in array) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = array[0];
+  for (let i in array) {
+    i > 0 ? result *= array[i] : result;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = a;
+  if (a === 0) {
+    return 1;
+  }
+  for (let i = a - 1; i > 0; --i) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
