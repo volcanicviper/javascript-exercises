15d8c29da32d774b93e64d838996425db8d16a40
Aleksander Kowalski <108731948+volcanicviper@users.noreply.github.com>
2022-08-22 19:27:50 +0200

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let fiboA = 0;
+    let fiboB = 1;
+    for (let i = 1; i < n; ++i) {
+        fiboB += fiboA;
+        fiboA = fiboB - fiboA;
+    }
+    return fiboB;
 };
 
 // Do not edit below this line
